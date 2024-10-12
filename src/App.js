30546c40d409d4951a210cb2d0917e656934869f
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-12 11:52:42 +0530

    adding images and development server serving images based on sizes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+import base64img from "./base64toimage";
+console.log(AlexaImage);
+console.log(base64img);
 
 function App(){
     return (
         <div>
             <div>Personal Digital Assistants</div>
+
+            <img src={AlexaImage} />
+            <img src={base64img} />
+
             <ProfileCard title="Alexa" handle="@alexa99" />
             <ProfileCard title="Cortana" handle="@cortana32" />
             <ProfileCard title="Siri" handle="@siri01"/>
