92c0ec422f8be773751afa5c03f07e1129e95662
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-12 12:33:32 +0530

    styling as per BULMA documentation to get CSS working

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -10,10 +10,18 @@ function ProfileCard({title, handle, image}){
     // 2. 'keys' into 1 line variables
     // const {title, handle}=props;
     return (
-        <div>
-            <img src={image} alt="pda logo"/>
-            <div>Title is {title}</div>
-            <div>Handle is {handle}</div>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt="pda logo"/>
+                </figure>
+            </div>
+            <div className="card-cotent">
+                <div className="media-content">
+                    <p className="title is-4">Title is {title}</p>
+                    <p className="subtitle is-6">Handle is {handle}</p>
+                </div>
+            </div>
 
         </div>
     );
